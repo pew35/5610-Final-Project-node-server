68bcda2d96a1142a2f5896e53838036278164fef
pew35 <peggywu226@gmail.com>
2024-12-04 16:50:58 -0800

    project update to A5

--- Kanbas/Courses/dao.js
+++ Kanbas/Courses/dao.js
@@ -1,3 +1,5 @@
+import Database from "../Database/index.js";
+
 export function findCoursesForEnrolledUser(userId) {
     const { courses, enrollments } = Database;
     const enrolledCourses = courses.filter((course) =>
@@ -8,4 +10,22 @@ export function createCourse(course) {
     const newCourse = { ...course, _id: Date.now().toString() };
     Database.courses = [...Database.courses, newCourse];
     return newCourse;
+}
+
+export function deleteCourse(courseId) {
+    const { courses, enrollments } = Database;
+    Database.courses = courses.filter((course) => course._id !== courseId);
+    Database.enrollments = enrollments.filter(
+        (enrollment) => enrollment.course !== courseId
+    );
+}
+
+export function updateCourse(courseId, courseUpdates) {
+    const { courses } = Database;
+    const course = courses.find((course) => course._id === courseId);
+    Object.assign(course, courseUpdates);
+    return course;
+}
+export function findAllCourses() {
+    return Database.courses;
 }
