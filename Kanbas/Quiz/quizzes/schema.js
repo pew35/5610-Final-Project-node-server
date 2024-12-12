23428e427561e3feb47c10991a41defa5d0cfa5c
jeff8971 <zhao.yuan2@northeastern.edu>
2024-12-12 04:35:37 -0500

    update quiz

--- Kanbas/Quiz/quizzes/schema.js
+++ Kanbas/Quiz/quizzes/schema.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const quizSchema = new mongoose.Schema({
     _id: String,
     title: String,
-    description: String,
-    publish: {type:Boolean, default: false},
+    instructions: String,
+    publish: {type: Boolean, default: false},
     attempts: Number,
     availableDate: String,
     availableUntilDate: String,
@@ -12,8 +12,27 @@ const quizSchema = new mongoose.Schema({
     points: Number,
     numberOfQuestions: Number,
     timeLimit: Number,
-    courseId: String
-  },
-  { collection: "quizzes" }
+    courseId: String,
+    quizType: {
+        type: String, 
+        enum: ['Graded Quiz', 'Practice Quiz', 'Graded Survey', 'Ungraded Survey'],
+        default: 'Graded Quiz'
+    },
+    assignmentGroup: {
+        type: String,
+        enum: ['Quizzes','Exams', 'Assignments', 'Project'],
+        default: 'Quizzes'
+    },
+    shuffleAnswers: {type: Boolean, default: false},
+    viewResponses: { type: String, default: 'Always' },
+    showCorrectAnswers: { type: String, default: 'Immediately' },
+    oneQuestionAtATime: { type: Boolean, default: true },
+    requireRespondusLockDown: { type: Boolean, default: false },
+    requiredToViewResults: { type: Boolean, default: false },
+    webcamRequired: { type: Boolean, default: false },
+    lockQuestionsAfterAnswering: { type: Boolean, default: false }
+}, 
+{ collection: "quizzes" }
 );
+
 export default quizSchema;
